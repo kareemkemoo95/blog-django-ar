14fe1382b11fe2053442f8f1138e822788857b0d
kareemkemoo95 <karimabdelrazik3@gmail.com>
2020-05-03 00:10:00 +0200

    design-my website

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import home
+from .views import home,about,post_detail
 
 urlpatterns = [
     path('',home,name='home'),
+    path('about/',about,name='about'),
+    path('detail/<int:post_id>/',post_detail,name='detail'),
+
+
+
 
 
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+
+from .models import Post
+from  .forms import NewComment
 
 # Create your views here.
 
+
+
+
 def home(request):
 
-    return  render(request,'index.html',{})
+    context = {
+        'title': 'الصفحة الرئيسية',
+        'posts':Post.objects.all(),
+        
+    }
+    return render(request, 'index.html', context)
+
+
+
+    # about
+def about(request):
+
+    return render(request, 'about.html', {'title': 'من أنا'})
+
+
+# post details or contents:
+def post_detail(request,post_id):
+    post= get_object_or_404(Post,pk=post_id)
+    comments = post.comments.filter(active=True)
+    comment_form=NewComment()
+    context={
+
+        'title':post,
+        'post':post,
+        'comments': comments,
+        'comment_form':comment_form,
+    }
+    return render(request,'detail.html',context)
